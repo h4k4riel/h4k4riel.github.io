c68d720dfa9af20711d7246b90123d686c520c68
david maya <davidmaya79@gmail.com>
2025-04-03 23:28:46 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'h4k4riel.github.io',
+    site: 'https://h4k4riel.github.io',
     vite: {
         plugins: [tailwindcss()]
     },
