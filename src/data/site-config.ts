eccb2343cbe7756511f82666b326e655e76710b8
david maya <davidmaya79@gmail.com>
2025-04-05 18:10:40 -0300

    Updates hero section with new bio
    Updates the hero section to reflect the new bio information and skills.
    This change personalizes the introduction with relevant details.

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -96,7 +96,7 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        text: "I'm **David Maya**, Soy Técnico en Informática y Computación a nivel superior y actualmente me enfoco en el área de Ciberseguridad. Estoy en constante aprendizaje de nuevas tecnologías como Linux, Docker, SQL, GitHub, Python y Bash Scripting. La educación continua y la adquisición de conocimiento son pilares fundamentales para mí. <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
         image: {
             src: '/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'
